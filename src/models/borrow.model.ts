524af31d53cc04c8e74c8a14d9a176529ba8351b
halaluddin2000 <helaluddin7001@gmail.com>
2025-09-16 12:16:47 +0600

    borrows controllers added

--- src/models/borrow.model.ts
+++ src/models/borrow.model.ts
@@ -1,15 +1,17 @@
-import { model, Schema, Types } from "mongoose";
+import { Schema, model, Document, Types } from "mongoose";
 
-export const borrowSchema = new Schema(
-  {
-    book: { type: Types.ObjectId, ref: "Book", required: true },
-    quantity: { type: Number, required: true, min: 1 },
-    dueDate: {
-      type: Date,
-      required: true,
-    },
-  },
-  { timestamps: true, versionKey: false }
-);
+interface IBorrow extends Document {
+  book: Types.ObjectId;
+  quantity: number;
+  dueDate: Date;
+  borrowedAt: Date;
+}
 
-export const Borrow = model("Borrow", borrowSchema);
+const borrowSchema = new Schema<IBorrow>({
+  book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
+  quantity: { type: Number, required: true, min: 1 },
+  dueDate: { type: Date, required: true },
+  borrowedAt: { type: Date, default: Date.now },
+});
+
+export const Borrow = model<IBorrow>("Borrow", borrowSchema);
