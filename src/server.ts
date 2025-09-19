2836238959033ca32e9bad317db73a5708e3dc26
halaluddin2000 <helaluddin7001@gmail.com>
2025-09-20 01:34:12 +0600

    bd url added

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
+require("dotenv").config();
 import { Server } from "http";
 import mongoose from "mongoose";
+
 import app from "./app";
 
 let server: Server;
@@ -11,9 +13,8 @@ const PORT = 5000;
 
 async function main() {
   try {
-    await mongoose.connect(
-      "mongodb+srv://library_management_System:dminmecndYjPHNj7@cluster0.jao09dz.mongodb.net/library_management_System?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.DB_URL as string);
+    console.log(process.env.DB_URL);
     console.log("Connected to MongoDB Using Mongoose!!");
     server = app.listen(PORT, () => {
       console.log(`App is listening on port ${PORT}`);
